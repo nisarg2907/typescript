ccc248d6f049d8876d6afaaa065b5cb6f06e5588
Nisarg <nisargshah.ict19@gmail.com>
2024-02-14 12:12:52 +0530

    init commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+const x = 1;
+console.log(x);
+const geetring = function (firstname) {
+    console.log("Hello", firstname);
+};
+geetring("Nisarg");
---
+++ main.ts
@@ -0,0 +1,7 @@
+const x : number = 1;
+console.log(x);
+
+const geetring = function(firstname : string){
+    console.log("Hello",firstname);
+}
+geetring("Nisarg");
